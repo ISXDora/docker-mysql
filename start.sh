1793d6e95e59e82762680124a6c0c0b46d26099b
Isadora Xavier <66398122+ISXDora@users.noreply.github.com>
2024-03-01 00:50:41 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@
 echo "Executando container docker com o banco de dados Mysql"
 cd ./
 sudo chmod 700 ./start.sh
-
 docker container run --name container-mysql -p 8084:3306 -e MYSQL_ROOT_PASSWORD="docker_pwd" -e MYSQL_DATABASE="docker_db" -e MYSQL_USER="docker_usr" -e MYSQL_PASSWORD="docker_pwd" -d mysql
 
 echo "Banco de dados iniciado!"
